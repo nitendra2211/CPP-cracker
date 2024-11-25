eca2c148f87665cf70c4d030928bf605c7074002
Nitendra Singh <124700050+nitendra2211@users.noreply.github.com>
2024-11-25 16:48:54 +0530

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,30 @@
+// First cpp program printing Hello World! -------------------------------------------------------------
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+//Output-----------
+Hello, World!
+
+
+
+
+
+// Print some more sentences in c++ -------------------------------------------------------------
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Hello, My name is Nitendra Singh and I love to code in C++" << endl;  //endl- is used for end of line.
+    cout << "C++ is a very easy to learn Programming language" << "\n"; //backslash \n is also used to end the line.
+    cout << "C++ is the only language which if you try you love it" << endl;
+    return 0;
+}
+
+//Output--------------
+Hello, My name is Nitendra Singh and I love to code in C++
+C++ is a very easy to learn Programming language
+C++ is the only language which if you try you love it
