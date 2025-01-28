5783c78713e3fbcd763dcd7a0ddcc4df46893dea
Nitendra Singh <124700050+nitendra2211@users.noreply.github.com>
2025-01-28 20:40:05 +0530

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -1,1 +1,17 @@
+// Taking input from the user
 
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cin >> a;
+cout << "Enter the Value of a" << endl;
+
+return 0;
+
+//Input
+2
+
+//Output
+Enter the Value of a
+2
